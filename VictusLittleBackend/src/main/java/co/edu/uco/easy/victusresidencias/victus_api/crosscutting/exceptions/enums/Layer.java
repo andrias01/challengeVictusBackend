003fbf3ac623055c2d59c630bf1b095348059b19
Andres Velez <andres.velez5136@uco.net.co>
2025-11-08 18:42:50 -0500

    Listo para construir

--- VictusLittleBackend/src/main/java/co/edu/uco/easy/victusresidencias/victus_api/crosscutting/exceptions/enums/Layer.java
+++
@@ -1,7 +0,0 @@
-package co.edu.uco.easy.victusresidencias.victus_api.crosscutting.exceptions.enums;
-
-public enum Layer {
-	
-	GENERAL, DATA, BUSINESSLOGIC, CONTROLLER, DTO, ENTITY, DOMAIN
-
-}
--- VictusLittleBackend/src/main/java/co/edu/uco/easy/victusresidencias/victus_api/dao/DeleteDAO.java
+++
@@ -1,6 +0,0 @@
-package co.edu.uco.easy.victusresidencias.victus_api.dao;
-
-interface DeleteDAO<T> {
-	void delete(T data);
-
-}
