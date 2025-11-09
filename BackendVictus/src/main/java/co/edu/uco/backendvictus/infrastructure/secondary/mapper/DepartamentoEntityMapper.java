b473d2e0857974eea3081a2385a3fed7b5812e96
Andres Velez <andres.velez5136@uco.net.co>
2025-11-08 23:15:29 -0500

    DepartamentoMapperEntity por definir

--- BackendVictus/src/main/java/co/edu/uco/backendvictus/infrastructure/secondary/mapper/DepartamentoEntityMapper.java
+++ BackendVictus/src/main/java/co/edu/uco/backendvictus/infrastructure/secondary/mapper/DepartamentoEntityMapper.java
@@ -1,28 +1,12 @@
 package co.edu.uco.backendvictus.infrastructure.secondary.mapper;
 
-import org.springframework.stereotype.Component;
-
 import co.edu.uco.backendvictus.domain.model.Departamento;
 import co.edu.uco.backendvictus.domain.model.Pais;
-<<<<<<< HEAD
-import co.edu.uco.backendvictus.infrastructure.secondary.entity.DepartamentoJpaEntity;
-
-=======
+import org.springframework.stereotype.Component;
 import co.edu.uco.backendvictus.infrastructure.secondary.entity.DepartamentoEntity;
->>>>>>> e0d19b829e9b2fc6b11580c19e3126a1f85fb997
 
 @Component
 public class DepartamentoEntityMapper {
-
-<<<<<<< HEAD
-    @Autowired
-    private PaisEntityMapper paisEntityMapper;
-
-    public abstract DepartamentoJpaEntity toEntity(Departamento departamento);
-
-    public Departamento toDomain(final DepartamentoJpaEntity entity) {
-        if (entity == null) {
-=======
     public DepartamentoEntity toEntity(final Departamento departamento) {
         if (departamento == null) {
             return null;
@@ -33,9 +17,8 @@ public class DepartamentoEntityMapper {
 
     public Departamento toDomain(final DepartamentoEntity entity, final Pais pais) {
         if (entity == null || pais == null) {
->>>>>>> e0d19b829e9b2fc6b11580c19e3126a1f85fb997
             return null;
         }
-        return Departamento.create(entity.getId(), entity.getNombre(), pais, entity.isActivo());
+        return null;
     }
 }
